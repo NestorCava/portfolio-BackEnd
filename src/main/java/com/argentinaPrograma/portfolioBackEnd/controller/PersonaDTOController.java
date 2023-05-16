edde32935dc2fa2605b158f87a354b9a05cda010
NestorCava <nestorcavallini@gmail.com>
2023-05-15 21:19:05 -0300

    Correccion del error de cors

--- src/main/java/com/argentinaPrograma/portfolioBackEnd/controller/PersonaDTOController.java
+++ src/main/java/com/argentinaPrograma/portfolioBackEnd/controller/PersonaDTOController.java
@@ -16,6 +16,7 @@ import com.argentinaPrograma.portfolioBackEnd.service.ISkillService;
 import java.util.ArrayList;
 import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -24,6 +25,7 @@ import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@CrossOrigin(origins = "http://localhost:4200")
 public class PersonaDTOController {
     @Autowired
     private IPersonaService personaService;
@@ -82,5 +84,4 @@ public class PersonaDTOController {
         
         return personaDTO;
     }
-    
 }
